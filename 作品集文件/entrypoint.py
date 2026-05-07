236f8226895143dc3f8cc5cce6aa9f9fb02c1d7b
Lancelin-T <2370938813@qq.com>
2026-05-07 19:28:35 +0800

    Update entrypoint.py

--- 作品集文件/entrypoint.py
+++ 作品集文件/entrypoint.py
@@ -6,5 +6,5 @@ page_potprocess_plotly = st.Page("pot_process_plotly/index.py", url_path="pot_pr
 page_mark_tool = st.Page("标签工具/page.py", url_path="mark_tool", title="标签工具", icon=":material/edit:")
 
 pg = st.navigation([page_potvolt, page_potprocess, page_potprocess_plotly, page_mark_tool])
-st.set_page_config(page_title="魏桥智铝-铝一 一分厂", page_icon=":material/home:", layout="wide")
+st.set_page_config(page_title="智铝分析可视化界面", page_icon=":material/home:", layout="wide")
 pg.run()
